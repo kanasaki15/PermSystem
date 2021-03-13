99288b5e269674b27a54b53cceeb68d3b037770c
kanasaki15 <kassi_0123@kassi-hp.info>
2021-03-13 18:58:05 +0900

    ほぼ完

---
+++ src/main/java/xyz/n7mn/dev/permsystem/PermListener.java
@@ -0,0 +1,111 @@
+package xyz.n7mn.dev.permsystem;
+
+import net.luckperms.api.LuckPerms;
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.RegisteredServiceProvider;
+
+import java.sql.*;
+
+public class PermListener implements Listener {
+
+    private final Plugin plugin;
+    private Connection con;
+
+    public PermListener(Plugin plugin, Connection con){
+        this.plugin = plugin;
+        this.con = con;
+    }
+
+    @EventHandler
+    public void PlayerJoinEvent (PlayerJoinEvent e){
+
+        new Thread(()->{
+            try {
+                PreparedStatement statement = con.prepareStatement("SELECT * FROM MinecraftUserList");
+                statement.execute();
+                statement.close();
+            } catch (SQLException ex){
+                try {
+                    con = DriverManager.getConnection("jdbc:mysql://" + plugin.getConfig().getString("mysqlServer") + ":" + plugin.getConfig().getInt("mysqlPort") + "/" + plugin.getConfig().getString("mysqlDatabase") + plugin.getConfig().getString("mysqlOption"), plugin.getConfig().getString("mysqlUsername"), plugin.getConfig().getString("mysqlPassword"));
+                    con.setAutoCommit(true);
+                } catch (SQLException ex2){
+                    plugin.getPluginLoader().disablePlugin(plugin);
+                }
+            }
+
+            String perm = "";
+            try {
+                PreparedStatement statement = con.prepareStatement("SELECT * FROM MinecraftUserList WHERE MinecraftUserID = ?");
+                statement.setString(1, e.getPlayer().getUniqueId().toString());
+                ResultSet set = statement.executeQuery();
+                if (set.next()){
+                    perm = set.getString("Role").toLowerCase();
+                }
+                set.close();
+                statement.close();
+            } catch (Exception ex){
+                ex.printStackTrace();
+                plugin.getPluginLoader().disablePlugin(plugin);
+            }
+
+            RegisteredServiceProvider<LuckPerms> provider = Bukkit.getServicesManager().getRegistration(LuckPerms.class);
+            if (provider != null) {
+                LuckPerms api = provider.getProvider();
+
+                api.getUserManager().getUser(e.getPlayer().getUniqueId()).setPrimaryGroup(perm);
+                return;
+            }
+
+            int permLevel = -1;
+            int userLevel = -1;
+
+            if (plugin.getConfig().getString("isOp").equals("admin")){
+                permLevel = 5;
+            }
+            if (plugin.getConfig().getString("isOp").equals("moderator")){
+                permLevel = 4;
+            }
+            if (plugin.getConfig().getString("isOp").equals("developer")){
+                permLevel = 3;
+            }
+            if (plugin.getConfig().getString("isOp").equals("mapper")){
+                permLevel = 2;
+            }
+            if (plugin.getConfig().getString("isOp").equals("Authenticated")){
+                permLevel = 1;
+            }
+
+            if (perm.equals("admin")){
+                userLevel = 5;
+            }
+            if (perm.equals("moderator")){
+                userLevel = 4;
+            }
+            if (perm.equals("developer")){
+                userLevel = 3;
+            }
+            if (perm.equals("mapper")){
+                userLevel = 2;
+            }
+            if (perm.equals("authenticated")){
+                userLevel = 1;
+            }
+
+            e.getPlayer().setOp(userLevel >= permLevel);
+        }).start();
+
+    }
+
+    @EventHandler
+    public void PlayerQuitEvent(PlayerQuitEvent e){
+        if (e.getPlayer().isOp()){
+            e.getPlayer().setOp(false);
+        }
+    }
+
+}
--- src/main/java/xyz/n7mn/dev/permsystem/PermSystem.java
+++ src/main/java/xyz/n7mn/dev/permsystem/PermSystem.java
@@ -2,13 +2,30 @@ package xyz.n7mn.dev.permsystem;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public final class PermSystem extends JavaPlugin {
+
+    private Connection con;
 
     @Override
     public void onEnable() {
         // Plugin startup logic
+        saveDefaultConfig();
 
-        
+        try {
+            DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+            con = DriverManager.getConnection("jdbc:mysql://" + getConfig().getString("mysqlServer") + ":" + getConfig().getInt("mysqlPort") + "/" + getConfig().getString("mysqlDatabase") + getConfig().getString("mysqlOption"), getConfig().getString("mysqlUsername"), getConfig().getString("mysqlPassword"));
+            con.setAutoCommit(true);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            getPluginLoader().disablePlugin(this);
+            return;
+        }
+
+        getServer().getPluginManager().registerEvents(new PermListener(this, con), this);
     }
 
     @Override
