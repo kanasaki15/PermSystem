1a01736b234833f1084d62695cf86977a4820d1c
kanasaki15 <kassi_0123@kassi-hp.info>
2021-03-13 13:51:15 +0900

    Initial commit

---
+++ src/main/java/xyz/n7mn/dev/permsystem/PermSystem.java
@@ -0,0 +1,17 @@
+package xyz.n7mn.dev.permsystem;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PermSystem extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
