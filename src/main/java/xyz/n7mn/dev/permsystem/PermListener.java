b324ea3e6c40cc54a9f735399cfeb3782b042941
kanasaki15 <kassi_0123@kassi-hp.info>
2021-03-21 04:12:48 +0900

    内部仕様変更に伴う改修

--- src/main/java/xyz/n7mn/dev/permsystem/PermListener.java
+++ src/main/java/xyz/n7mn/dev/permsystem/PermListener.java
@@ -3,14 +3,12 @@ package xyz.n7mn.dev.permsystem;
 import net.luckperms.api.LuckPerms;
 import net.luckperms.api.model.user.User;
 import net.luckperms.api.node.Node;
-import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.Plugin;
-import org.bukkit.plugin.RegisteredServiceProvider;
 
 import java.sql.*;
 
@@ -18,102 +16,91 @@ class PermListener implements Listener {
 
     private final Plugin plugin;
     private Connection con;
+    private final LuckPerms luckPerms;
 
-    public PermListener(Plugin plugin, Connection con){
+    public PermListener(Plugin plugin, Connection con, LuckPerms luckPerms){
         this.plugin = plugin;
         this.con = con;
+        this.luckPerms = luckPerms;
+    }
+
+    public PermListener(PermSystem plugin, Connection con) {
+        this.plugin = plugin;
+        this.con = con;
+        this.luckPerms = null;
     }
 
     @EventHandler
     public void PlayerJoinEvent (PlayerJoinEvent e){
 
-        new Thread(()->{
+        try {
+            PreparedStatement statement = con.prepareStatement("SELECT * FROM MinecraftUserList");
+            statement.execute();
+            statement.close();
+        } catch (SQLException ex){
             try {
-                PreparedStatement statement = con.prepareStatement("SELECT * FROM MinecraftUserList");
-                statement.execute();
-                statement.close();
-            } catch (SQLException ex){
-                try {
-                    con = DriverManager.getConnection("jdbc:mysql://" + plugin.getConfig().getString("mysqlServer") + ":" + plugin.getConfig().getInt("mysqlPort") + "/" + plugin.getConfig().getString("mysqlDatabase") + plugin.getConfig().getString("mysqlOption"), plugin.getConfig().getString("mysqlUsername"), plugin.getConfig().getString("mysqlPassword"));
-                    con.setAutoCommit(true);
-                } catch (SQLException ex2){
-                    plugin.getPluginLoader().disablePlugin(plugin);
-                }
-            }
-
-            String perm = "";
-
-            try {
-                PreparedStatement statement = con.prepareStatement("SELECT * FROM MinecraftUserList WHERE MinecraftUUID = ?");
-                statement.setString(1, e.getPlayer().getUniqueId().toString());
-                ResultSet set = statement.executeQuery();
-                if (set.next()){
-                    perm = set.getString("Role").toLowerCase();
-                }
-                set.close();
-                statement.close();
-            } catch (Exception ex){
-                ex.printStackTrace();
+                con = DriverManager.getConnection("jdbc:mysql://" + plugin.getConfig().getString("mysqlServer") + ":" + plugin.getConfig().getInt("mysqlPort") + "/" + plugin.getConfig().getString("mysqlDatabase") + plugin.getConfig().getString("mysqlOption"), plugin.getConfig().getString("mysqlUsername"), plugin.getConfig().getString("mysqlPassword"));
+                con.setAutoCommit(true);
+            } catch (SQLException ex2){
+                ex2.printStackTrace();
                 plugin.getPluginLoader().disablePlugin(plugin);
             }
+        }
 
-            if (Bukkit.getServer().getPluginManager().getPlugin("LuckPerms") != null){
-                try {
-                    RegisteredServiceProvider<LuckPerms> provider = Bukkit.getServicesManager().getRegistration(LuckPerms.class);
-                    if (provider != null) {
-                        LuckPerms api = provider.getProvider();
+        String perm = "";
+        int userLevel = 0;
 
-                        User user = api.getPlayerAdapter(Player.class).getUser(e.getPlayer());
+        try {
+            PreparedStatement statement = con.prepareStatement("" +
+                    "SELECT * FROM MinecraftUserList a, RoleRankList b WHERE a.RoleUUID = b.UUID" +
+                    " AND MinecraftUUID = ?"
+            );
+            statement.setString(1, e.getPlayer().getUniqueId().toString());
+            ResultSet set = statement.executeQuery();
+            if (set.next()){
+                perm = set.getString("Name").toLowerCase();
+                userLevel = set.getInt("Rank");
+            }
+            set.close();
+            statement.close();
+        } catch (Exception ex){
+            ex.printStackTrace();
+            plugin.getPluginLoader().disablePlugin(plugin);
+        }
 
-                        user.data().clear();
-                        user.data().add(Node.builder("group."+perm).build());
+        try {
+            if (luckPerms != null){
+                User user = luckPerms.getPlayerAdapter(Player.class).getUser(e.getPlayer());
 
-                        api.getUserManager().saveUser(user);
-                        return;
-                    }
-                } catch (Exception ex){
-                    ex.printStackTrace();
-                }
-            }
+                user.data().clear();
+                user.data().add(Node.builder("group."+perm).build());
 
-            int permLevel = -1;
-            int userLevel = -1;
+                luckPerms.getUserManager().saveUser(user);
+                return;
+            }
+        } catch (Exception ex){
+            ex.printStackTrace();
+        }
 
-            if (plugin.getConfig().getString("isOp").equals("admin")){
-                permLevel = 5;
-            }
-            if (plugin.getConfig().getString("isOp").equals("moderator")){
-                permLevel = 4;
-            }
-            if (plugin.getConfig().getString("isOp").equals("developer")){
-                permLevel = 3;
-            }
-            if (plugin.getConfig().getString("isOp").equals("mapper")){
-                permLevel = 2;
-            }
-            if (plugin.getConfig().getString("isOp").equals("Authenticated")){
-                permLevel = 1;
-            }
+        int permLevel = -1;
 
-            if (perm.equals("admin")){
-                userLevel = 5;
-            }
-            if (perm.equals("moderator")){
-                userLevel = 4;
-            }
-            if (perm.equals("developer")){
-                userLevel = 3;
-            }
-            if (perm.equals("mapper")){
-                userLevel = 2;
-            }
-            if (perm.equals("authenticated")){
-                userLevel = 1;
-            }
+        if (plugin.getConfig().getString("isOp").equals("admin")){
+            permLevel = 5;
+        }
+        if (plugin.getConfig().getString("isOp").equals("moderator")){
+            permLevel = 4;
+        }
+        if (plugin.getConfig().getString("isOp").equals("developer")){
+            permLevel = 3;
+        }
+        if (plugin.getConfig().getString("isOp").equals("mapper")){
+            permLevel = 2;
+        }
+        if (plugin.getConfig().getString("isOp").equals("Authenticated")){
+            permLevel = 1;
+        }
 
-            e.getPlayer().setOp(userLevel >= permLevel);
-        }).start();
-
+        e.getPlayer().setOp(permLevel <= userLevel);
     }
 
     @EventHandler
--- src/main/java/xyz/n7mn/dev/permsystem/PermSystem.java
+++ src/main/java/xyz/n7mn/dev/permsystem/PermSystem.java
@@ -1,5 +1,9 @@
 package xyz.n7mn.dev.permsystem;
 
+import net.luckperms.api.LuckPerms;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.sql.Connection;
@@ -15,20 +19,35 @@ public final class PermSystem extends JavaPlugin {
         // Plugin startup logic
         saveDefaultConfig();
 
-        new Thread(()->{
-            try {
-                DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
-                con = DriverManager.getConnection("jdbc:mysql://" + getConfig().getString("mysqlServer") + ":" + getConfig().getInt("mysqlPort") + "/" + getConfig().getString("mysqlDatabase") + getConfig().getString("mysqlOption"), getConfig().getString("mysqlUsername"), getConfig().getString("mysqlPassword"));
-                con.setAutoCommit(true);
-            } catch (SQLException e) {
-                e.printStackTrace();
-                getPluginLoader().disablePlugin(this);
-                return;
+        LuckPerms api = null;
+        Plugin[] plugins = getServer().getPluginManager().getPlugins();
+        for (Plugin plugin : plugins){
+            System.out.println(plugin.getName());
+            if (!plugin.getName().equals("LuckPerms")){
+                continue;
             }
 
-            getServer().getPluginManager().registerEvents(new PermListener(this, con), this);
-        }).start();
+            System.out.println("？");
+            try {
+                RegisteredServiceProvider<LuckPerms> provider = Bukkit.getServicesManager().getRegistration(LuckPerms.class);
+                if (provider != null) {
+                    api = provider.getProvider();
+                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
 
+        try {
+            DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+            con = DriverManager.getConnection("jdbc:mysql://" + getConfig().getString("mysqlServer") + ":" + getConfig().getInt("mysqlPort") + "/" + getConfig().getString("mysqlDatabase") + getConfig().getString("mysqlOption"), getConfig().getString("mysqlUsername"), getConfig().getString("mysqlPassword"));
+            con.setAutoCommit(true);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            getPluginLoader().disablePlugin(this);
+        }
+
+        getServer().getPluginManager().registerEvents(new PermListener(this, con), this);
     }
 
     @Override
